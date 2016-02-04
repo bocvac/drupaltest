b080e3ef9a8b40037e2100116f1b2f56da43f944
Brant Wynn <brant.wynn@acquia.com>
2016-02-04 16:42:04 -0600

    Added basic user migration.

--- import.example.sh
+++ import.example.sh
@@ -9,5 +9,7 @@ echo "Running migration imports..."
 drush mi import_node_page
 drush mi import_term_tags
 drush mi import_node_article
+drush mi import_file_image
+drush mi import_user_user
 echo "Import migrations were attempted."
 echo "If there are no errors, you may proceed."
