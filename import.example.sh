ca9491ef1e225a5c5ccd67a3a48fc4c6e3933788
Brant Wynn <brant.wynn@acquia.com>
2016-03-11 16:31:26 -0600

    Add import_comment_comment to testing example

--- import.example.sh
+++ import.example.sh
@@ -10,6 +10,7 @@ echo "Running migration imports..."
 drush mi import_node_page
 drush mi import_term_tags
 drush mi import_node_article
+drush mi import_comment_comment
 drush mi import_file_image
 drush mi import_user_user
 echo "Import migrations were attempted."
